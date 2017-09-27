c8a7ad9353a5427374378004ccdff39487496a5f
Laurent Parisot <parisot.laurent@gmail.com>
2017-09-27 21:36:21 +0200

    MaterialModule is deprecated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 //import { FormsModule } from '@angular/forms';
 //import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material';
+//import { MaterialModule } from '@angular/material';
+import { MaterialModule } from './material/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import 'hammerjs';
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  MdButtonModule,
+  MdMenuModule,
+  MdToolbarModule,
+  MdIconModule,
+  MdCardModule,
+  MdGridListModule,
+  MdListModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MdButtonModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdCardModule,
+    MdGridListModule,
+    MdListModule
+  ],
+  exports: [
+    MdButtonModule,
+    MdMenuModule,
+    MdToolbarModule,
+    MdIconModule,
+    MdCardModule,
+    MdGridListModule,
+    MdListModule
+  ],
+  declarations: []
+})
+
+export class MaterialModule { }
